91d44f79705bbe188f3804bc13eb52ba85b52f77
Ali <ali.hassan.ashiry@gmail.com>
2025-08-09 15:18:04 +0300

    Add Categories List

--- Js/data.js
+++ Js/data.js
@@ -11,11 +11,17 @@ fetch("/Products_Json/products.json")
   .then((productData) => {
     localStorage.setItem("productsList", JSON.stringify(productData));
     console.log("Products data saved");
+  const categories = [...new Set(productData.map((item) => item.Category))];
+  localStorage.setItem("Categories", JSON.stringify(categories));
+  console.log("Unique Categories saved:", categories);
+    console.log("Categories saved:", categories);
   })
   .catch((error) => console.error("Error loading product.json:", error));
 
 const users = JSON.parse(localStorage.getItem("usersList"));
 const products = JSON.parse(localStorage.getItem("productsList"));
+const categories = JSON.parse(localStorage.getItem("Categories"));
 
 console.log(users);
 console.log(products);
+console.log(categories);
