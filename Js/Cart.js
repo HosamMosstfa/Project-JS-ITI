d7f5f60cb6f2c657b031e3c9405ebb1503ec7dcd
ManarElbadry <manarsaleh147@gmail.com>
2025-08-09 19:25:34 +0300

    navbar&footer

---
+++ Js/Cart.js
@@ -0,0 +1,12 @@
+//navbar.js
+
+document.addEventListener("DOMContentLoaded", () => {
+  const currentPage = window.location.pathname.split("/").pop() || "index.html";
+  
+  document.querySelectorAll(".navbar-nav .nav-link").forEach(link => {
+    if (link.getAttribute("href") === currentPage) {
+      link.classList.add("active");
+    }
+  });
+});
+
