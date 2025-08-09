7e4e353a34f459fd96fe1c06e089dbb408557432
Ali <ali.hassan.ashiry@gmail.com>
2025-08-09 14:21:05 +0300

    Add Data To Local Storage

---
+++ Js/data.js
@@ -0,0 +1,21 @@
+fetch("/user_json/users.json")
+  .then((response) => response.json())
+  .then((userData) => {
+    localStorage.setItem("usersList", JSON.stringify(userData));
+    console.log("Users data saved");
+  })
+  .catch((error) => console.error("Error loading user.json:", error));
+
+fetch("/Products_Json/products.json")
+  .then((response) => response.json())
+  .then((productData) => {
+    localStorage.setItem("productsList", JSON.stringify(productData));
+    console.log("Products data saved");
+  })
+  .catch((error) => console.error("Error loading product.json:", error));
+
+const users = JSON.parse(localStorage.getItem("usersList"));
+const products = JSON.parse(localStorage.getItem("productsList"));
+
+console.log(users);
+console.log(products);
